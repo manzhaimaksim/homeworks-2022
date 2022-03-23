8ba9d6fb0c5924f364f157201e553f9f3fc9ec6a
manzhaimaksim <maxbbb88@gmail.com>
2022-03-23 20:38:24 +0300

    initial commit

---
+++ spec/hw_04_mentor.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'rspec'
+require_relative 'spec_helper'
+require_relative 'support/reload_file'
+
+require_relative '../HW04/maksim_manzhai/lib/mentor.rb'
+
+describe Mentor do
+end
---
+++ spec/hw_04_notification.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'rspec'
+require_relative 'spec_helper'
+require_relative 'support/reload_file'
+
+require_relative '../HW04/maksim_manzhai/lib/student.rb'
+
+describe Student do
+end

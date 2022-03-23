3d1fd6d41a9bad5d0c849cf4fce992658cb5975e
manzhaimaksim <maxbbb88@gmail.com>
2022-03-23 20:37:09 +0300

    initial commit

--- HW04/maksim_manzhai/lib/homework.rb
+++ HW04/maksim_manzhai/lib/homework.rb
@@ -1,20 +1,20 @@
 # frozen_string_literal: true
 
 class Homework
-  attr_reader :title
-  attr_accessor :mentor, :answers, :student, :acceptable
+  # attr_reader :title
+  # attr_accessor :mentor, :answers, :student, :acceptable
 
-  def initialize(title:, description:, student:, mentor:)
-    @title = title
-    @description = description
-    @student = student
-    @mentor = mentor
-    send_notification
-    @answers = []
-    @acceptable = false
-  end
+  # def initialize(title:, description:, student:, mentor:)
+  #   @title = title
+  #   @description = description
+  #   @student = student
+  #   @mentor = mentor
+  #   send_notification
+  #   @answers = []
+  #   @acceptable = false
+  # end
 
-  def send_notification
-    @student.notifications << Notification.new(title: @title, description: @description)
-  end
+  # def send_notification
+  #   @student.notifications << Notification.new(title: @title, description: @description)
+  # end
 end
